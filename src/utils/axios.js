c939987fe31e2de107dc54ffcd7fc56942b8bd98
Ritesh Ranjan <84141214+Ritesh-1310@users.noreply.github.com>
2025-06-21 00:53:32 +0530

    changed the production url of backend

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
   baseURL: import.meta.env.PROD 
-    ? 'https://blogify-bice-rho.vercel.app/api' 
+    ? 'https://blogify-woad-three.vercel.app/api' 
     : 'http://localhost:8002/api',
   withCredentials: true,
 });
